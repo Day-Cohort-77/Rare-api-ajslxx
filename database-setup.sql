a8f6942abafa19db6591e636d3489ffa9acb046e
Stephen <peterson.stephen138@gmail.com>
2025-08-19 13:40:13 -0500

    corected async calls for database

--- database-setup.sql
+++ database-setup.sql
@@ -1,80 +1,80 @@
 CREATE TABLE "Users" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "first_name" varchar,
-  "last_name" varchar,
-  "email" varchar,
-  "bio" varchar,
-  "username" varchar,
-  "password" varchar,
-  "profile_image_url" varchar,
-  "created_on" date,
-  "active" bit
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  email VARCHAR,
+  bio VARCHAR,
+  username VARCHAR,
+  password VARCHAR,
+  profile_image_url VARCHAR,
+  created_on DATE,
+  active BOOLEAN
 );
 
 CREATE TABLE "Subscriptions" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "follower_id" INTEGER,
-  "author_id" INTEGER,
-  "created_on" date,
-  FOREIGN KEY(`follower_id`) REFERENCES `Users`(`id`),
-  FOREIGN KEY(`author_id`) REFERENCES `Users`(`id`)
+  id SERIAL PRIMARY KEY,
+  follower_id INTEGER,
+  author_id INTEGER,
+  created_on DATE,
+  FOREIGN KEY(follower_id) REFERENCES "Users"(id),
+  FOREIGN KEY(author_id) REFERENCES "Users"(id)
 );
 
 CREATE TABLE "Posts" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "user_id" INTEGER,
-  "category_id" INTEGER,
-  "title" varchar,
-  "publication_date" date,
-  "image_url" varchar,
-  "content" varchar,
-  "approved" bit,
-  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`)
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER,
+  category_id INTEGER,
+  title VARCHAR,
+  publication_date DATE,
+  image_url VARCHAR,
+  content VARCHAR,
+  approved BOOLEAN,
+  FOREIGN KEY(user_id) REFERENCES "Users"(id)
 );
 
 CREATE TABLE "Comments" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "post_id" INTEGER,
-  "author_id" INTEGER,
-  "content" varchar,
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`),
-  FOREIGN KEY(`author_id`) REFERENCES `Users`(`id`)
+  id SERIAL PRIMARY KEY,
+  post_id INTEGER,
+  author_id INTEGER,
+  content VARCHAR,
+  FOREIGN KEY(post_id) REFERENCES "Posts"(id),
+  FOREIGN KEY(author_id) REFERENCES "Users"(id)
 );
 
 CREATE TABLE "Reactions" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "label" varchar,
-  "image_url" varchar
+  id SERIAL PRIMARY KEY,
+  label VARCHAR,
+  image_url VARCHAR
 );
 
 CREATE TABLE "PostReactions" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "user_id" INTEGER,
-  "reaction_id" INTEGER,
-  "post_id" INTEGER,
-  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
-  FOREIGN KEY(`reaction_id`) REFERENCES `Reactions`(`id`),
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`)
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER,
+  reaction_id INTEGER,
+  post_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES "Users"(id),
+  FOREIGN KEY(reaction_id) REFERENCES "Reactions"(id),
+  FOREIGN KEY(post_id) REFERENCES "Posts"(id)
 );
 
 CREATE TABLE "Tags" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "label" varchar
+  id SERIAL PRIMARY KEY,
+  label VARCHAR
 );
 
 CREATE TABLE "PostTags" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "post_id" INTEGER,
-  "tag_id" INTEGER,
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`),
-  FOREIGN KEY(`tag_id`) REFERENCES `Tags`(`id`)
+  id SERIAL PRIMARY KEY,
+  post_id INTEGER,
+  tag_id INTEGER,
+  FOREIGN KEY(post_id) REFERENCES "Posts"(id),
+  FOREIGN KEY(tag_id) REFERENCES "Tags"(id)
 );
 
 CREATE TABLE "Categories" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "label" varchar
+  id SERIAL PRIMARY KEY,
+  label VARCHAR
 );
 
-INSERT INTO Categories ('label') VALUES ('News');
-INSERT INTO Tags ('label') VALUES ('JavaScript');
-INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
+INSERT INTO "Categories" (label) VALUES ('News');
+INSERT INTO "Tags" (label) VALUES ('JavaScript');
+INSERT INTO "Reactions" (label, image_url) VALUES ('happy', 'https://pngtree.com/so/happy');
